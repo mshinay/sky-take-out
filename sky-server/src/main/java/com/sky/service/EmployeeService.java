b1a9c7285a80c24241275b9a7b8f875808c5958b
mshinay511@gmail.com <mshinay511@gmail.com>
2025-03-17 14:52:05 +0800

    编辑账号密码

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -3,6 +3,7 @@ package com.sky.service;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.dto.EmployeePageQueryDTO;
+import com.sky.dto.PasswordEditDTO;
 import com.sky.entity.Employee;
 import com.sky.result.PageResult;
 
@@ -47,4 +48,10 @@ public interface EmployeeService {
      * @param employeeDTO
      */
     void updateEmployeeInfo(EmployeeDTO employeeDTO);
+
+    /**
+     * 编辑账号密码
+     * @param passwordEditDTO
+     */
+    void editPassword(PasswordEditDTO passwordEditDTO);
 }
